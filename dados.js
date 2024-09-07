096f2c5f01bd2f2eb70ee48e7ba9940870de5121
SU <sonianasc@gmx.at>
2024-09-08 01:29:51 +0200

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,67 @@
+let dados = [
+
+    {
+        titulo : "Plataforma Java",
+        descricao : "A plataforma Java é constituída de um grande número de tecnologias, cada uma provê uma porção distinta de todo o ambiente de desenvolvimento e execução de software. Os usuários finais, tipicamente, interagem com a máquina virtual Java (Java Virtual Machine, ou JVM) e um conjunto padrão de bibliotecas de classe.",
+        link : "https://pt.wikipedia.org/wiki/Java_(plataforma_de_software)",
+        tags: "Java java"
+    },
+    {
+        titulo : "HTML 5",
+        descricao : "HTML5 (Hypertext Markup Language, versão 5) é uma linguagem de marcação para a World Wide Web e é uma tecnologia chave da Internet",
+        link : "https://pt.wikipedia.org/wiki/HTML5",
+        tags: "HTML html htm"
+    },
+    {
+        titulo : "CSS",
+        descricao : "Cascading Style Sheets (abreviado CSS) é um mecanismo para adicionar estilos (cores, fontes, espaçamento, etc.) a uma página web.",
+        link : "https://pt.wikipedia.org/wiki/Cascading_Style_Sheets",
+        tags: "CSS css cs"
+    
+    },
+    {
+        titulo : "JavaScript",
+        descricao : "JavaScript (frequentemente abreviado como JS) é uma linguagem de programação interpretada estruturada, de script em alto nível com tipagem dinâmica fraca e multiparadigma (protótipos, orientado a objeto, imperativo e funcional).",
+        link : "https://pt.wikipedia.org/wiki/JavaScript",
+        tags: "JavaScript Javascript javascript JS js Js"
+    },
+    {
+        titulo : "Python",
+        descricao : "Python é uma linguagem de programação de alto nível,[5] interpretada, imperativa, orientada a objetos, funcional, de tipagem dinâmica e forte. Foi lançada por Guido van Rossum em 1991.",
+        link : "https://pt.wikipedia.org/wiki/Python",
+        tags: "Python python py"
+    },
+    {
+        titulo : "Linguagem R",
+        descricao : "R é uma linguagem de programação multi-paradigma orientada a objetos, programação funcional, dinâmica, fracamente tipada, voltada à manipulação, análise e visualização de dados.",
+        link : "https://pt.wikipedia.org/wiki/R_(linguagem_de_programa%C3%A7%C3%A3o)",
+        tags: "Linguagem R Linguagem r"
+    },
+    {
+        titulo : "Linguagem Go",
+        descricao : "Go é uma linguagem de programação criada pela Google e lançada em código livre em novembro de 2009. É uma linguagem compilada e focada em produtividade e programação concorrente,[6] baseada em trabalhos feitos no sistema operacional chamado Inferno.",
+        link : "https://pt.wikipedia.org/wiki/Go_(linguagem_de_programa%C3%A7%C3%A3o)",
+        tags: "GO go"
+    },
+    {
+        titulo : "Linguagem Script",
+        descricao : "Linguagem de script ou scripting é uma linguagem de programação que suporta scripts, programas escritos para um sistema de tempo de execução especial que automatiza a execução de tarefas que seriam executadas, uma de cada vez, por um operador humano.",
+        link : "https://pt.wikipedia.org/wiki/Linguagem_de_script",
+        tags: "Linguagem Script Linguagem script"
+    },
+    {
+        titulo : "Linguagem Programacao C",
+        descricao : "Linguagem de script ou scripting é uma linguagem de programação que suporta scripts, programas escritos para um sistema de tempo de execução especial que automatiza a execução de tarefas que seriam executadas, uma de cada vez, por um operador humano.",
+        link : "https://pt.wikipedia.org/wiki/C_(linguagem_de_programa%C3%A7%C3%A3o)",
+        tags: "Linguagem C Linguagem c"
+    },
+    {
+        titulo : "Linguagem C++",
+        descricao : "C++ (em português: Pronuncia-se cê mais mais) é uma linguagem de programação compilada multi-paradigma (seu suporte inclui linguagem imperativa, orientada a objetos e genérica) e de uso geral. Desde os anos 1990 é uma das linguagens comerciais mais populares, sendo bastante usada também na academia por seu grande desempenho e base de utilizadores.",
+        link : "https://pt.wikipedia.org/wiki/C%2B%2B",
+        tags: "Linguagem C++ Linguagem c++"
+    },
+];
+
+
+
